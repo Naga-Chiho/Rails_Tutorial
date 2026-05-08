ab86da0667501469d7fd2b48e69bd6963e0d1303
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-05-08 12:24:55 +0900

    ユーザーCRUD実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "up" => "rails/health#show", as: :rails_health_check
+  # get "/users", to: "users#index"
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
+  # get "/users/new", to: "users#new"
+  # post "/users", to: "users#create"
 
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
+  # get "/users/:id/edit", to: "users#edit"
+  # patch "/users/:id", to: "users#update"
+  # put "/users/:id", to: "users#update"
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # delete "/users/:id", to: "users#destroy"
+
+  # この子あれば一発よ
+  resources :users
 end
