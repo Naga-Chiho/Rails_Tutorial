f5fdf59e59ce05f93b43e6a37c967bf3ee00e432
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-05-15 16:05:52 +0900

    マイグレーションファイルの設定

--- db/migrate/20260507055750_create_users.rb
+++ db/migrate/20260507055750_create_users.rb
@@ -1,7 +1,19 @@
 class CreateUsers < ActiveRecord::Migration[8.1]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :name         
+      t.string :furigana      # rubi
+      t.string :gender        # seibetu
+      t.string :phone         # denwa
+      t.string :mobile_phone  # keitai
+      t.string :email         # mairu
+      t.string :postal_code   # yuubinbango
+      t.string :address1      # jusho1
+      t.string :address2      # jusho2
+      t.string :address3      # jusho3
+      t.string :address4      # jusho4
+      t.string :address5      # jusho5
+      t.date   :birthday      # tanjobi 
 
       t.timestamps
     end
