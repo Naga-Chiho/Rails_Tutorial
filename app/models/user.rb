59152cd9b25d0f3f186037d6c2f8fe2f8ba3e75d
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-05-18 17:28:48 +0900

    バリデーションファイルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,40 @@
 class User < ApplicationRecord
   validates :name, presence: true
+  validates :name, length: { in: 2..50 ,message: "文字数が不適切です"}
+
   validates :furigana, presence: true
+  validates :furigana, format: { with: /\A[ァ-ヴー]+\z/,message: "カタカナのみ使用できます" }
+
   validates :gender, presence: true
+
   validates :phone, presence: true
+  validates :phone, presence: true, format: { with: /\A0\d{2,4}-\d{1,4}-\d{1,4}\z/, message: "ハイフンありの電話番号でお願いします" }
+
+  validates :mobile_phone, format: { with: /\A(070|080|090)-\d{4}-\d{4}\z/, message: "ハイフンありの携帯番号でお願いします" }
+
+  validates :email, presence: true
+  validates :email, presence: true, format: { with: /\A[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}\z/ }
+
   validates :postal_code, presence: true
+  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/, message: "ハイフンありでお願いします" }
+
   validates :address1, presence: true
   validates :address2, presence: true
   validates :address3, presence: true
   validates :address4, presence: true
   validates :birthday, presence: true
 
-  validate :phone_or_mobile_phone
+  enum :address1, {
+    "北海道" => 1, "青森県" => 2, "岩手県" => 3, "宮城県" => 4, "秋田県" => 5,
+    "山形県" => 6, "福島県" => 7, "茨城県" => 8, "栃木県" => 9, "群馬県" => 10,
+    "埼玉県" => 11, "千葉県" => 12, "東京都" => 13, "神奈川県" => 14, "新潟県" => 15,
+    "富山県" => 16, "石川県" => 17, "福井県" => 18, "山梨県" => 19, "長野県" => 20,
+    "岐阜県" => 21, "静岡県" => 22, "愛知県" => 23, "三重県" => 24, "滋賀県" => 25,
+    "京都府" => 26, "大阪府" => 27, "兵庫県" => 28, "奈良県" => 29, "和歌山県" => 30,
+    "鳥取県" => 31, "島根県" => 32, "岡山県" => 33, "広島県" => 34, "山口県" => 35,
+    "徳島県" => 36, "香川県" => 37, "愛媛県" => 38, "高知県" => 39, "福岡県" => 40,
+    "佐賀県" => 41, "長崎県" => 42, "熊本県" => 43, "大分県" => 44, "宮崎県" => 45,
+    "鹿児島県" => 46, "沖縄県" => 47
+  }
+
 end
