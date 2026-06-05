fa3c180aafb26c7038dc6902604d251ad3e5d46e
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-06-05 16:50:07 +0900

    #20 ルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :departments
-    resources :users
     resources :skills
+    resources :users do
+      member do
+        get 'show_image' 
+      end
+    end
   end
 
     resources :departments, only: [:index , :show]
