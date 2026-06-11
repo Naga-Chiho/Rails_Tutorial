2d11f1ad3b3e16e018cb6ef5ab659ca35fd8d7f3
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-06-11 14:26:50 +0900

    #22 一般画面の絞り込み機能修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,10 @@ class UsersController < ApplicationController
       @users = User.where("prefecture LIKE ?", params[:prefecture])
     end
 
-    if params[:birthday_asc].present?
-      @users = User.order(birthday: :ASC)
-    end
-
-    if params[:birthday].present?
-      @users = User.order(birthday: :DESC)
+    if params[:birthday] == "asc"
+      @users = @users.order(birthday: :asc)
+    elsif params[:sort] == "desc"
+      @users = @users.order(birthday: :desc)
     end
 
     @users = @users.page(params[:page]).per(params[:per_page].presence)
