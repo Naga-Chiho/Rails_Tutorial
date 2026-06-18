cd782def44ee0069a5e64b64cc5fc700b8347f64
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-06-18 13:13:03 +0900

    #28　スキップアクションの追加

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,6 +1,8 @@
 # こやつは！！Adminのやつ！！！！！！！(メモ)
 module Admin 
   class SessionsController < ApplicationController
+    skip_before_action :require_login, only: [:new, :create]
+    
     def new
     end
 
