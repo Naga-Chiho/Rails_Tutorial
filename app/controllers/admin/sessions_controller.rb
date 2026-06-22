7553ef1685215182b1c3efdbcdb863bf7beff28b
Naga-Chiho <chiho.nagayama@rizapgroup.com>
2026-06-22 14:51:33 +0900

    #31 セッション固定攻撃を防ぐ

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -9,6 +9,8 @@ module Admin
       user = User.find_by(email: params[:email])
 
       if user && user.authenticate(params[:password])
+        reset_session 
+        
         session[:user_id] = user.id
         redirect_to admin_users_path
       else
